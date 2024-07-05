dcb04dd1f844498f7255b7cefea5b93a2f5bad5d
Aleksandra Allunan <sasha-soboleva@mail.ru>
2024-07-05 17:15:27 +0300

    Первая сдача

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const addButton = document.querySelector(".profile__add-button");
+const placeList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
+function deleteCard(event){
+  event.target.closest(".card").remove();
+}
+
+ // @todo: Функция создания карточки
+initialCards.forEach((item) => {
+  item.link, item.name;
+}); 
+function addCard(item) {
+  const card = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardImg = card.querySelector(".card__image");
+  const cardName = card.querySelector(".card__title");
+  const deleteButton = card.querySelector(".card__delete-button");
+  cardImg.src = item.link;
+  cardName.textContent = item.name;
+  deleteButton.addEventListener('click', deleteCard);
+  return card;
+
+}
+
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+  placeList.append(addCard(item));
+});

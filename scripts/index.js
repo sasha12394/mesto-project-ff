0b3e36bc1ae40a5a8608b80e4ffb8d8d169c28ee
Aleksandra Allunan <sasha-soboleva@mail.ru>
2024-07-05 17:56:41 +0300

    Добавлен alt

--- scripts/index.js
+++ scripts/index.js
@@ -14,15 +14,16 @@ function deleteCard(event){
 
  // @todo: Функция создания карточки
 initialCards.forEach((item) => {
-  item.link, item.name;
+  item.link, item.name
 }); 
-function addCard(item) {
+function addCard(item, deleteCard) {
   const card = cardTemplate.querySelector(".card").cloneNode(true);
   const cardImg = card.querySelector(".card__image");
   const cardName = card.querySelector(".card__title");
   const deleteButton = card.querySelector(".card__delete-button");
   cardImg.src = item.link;
   cardName.textContent = item.name;
+  cardImg.alt = item.name;
   deleteButton.addEventListener('click', deleteCard);
   return card;
 
